294fad039ac336e70cd6b8707578008bfcd229c5
Constantin Utner <constantin.utner@stud.fh-campuswien.ac.at>
2026-04-08 09:56:44 +0200

    Initial commit

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private float platformSpeed;
+
+    [SerializeField]
+    private Vector3 start;
+
+    [SerializeField]
+    private Vector3 end;
+
+    void FixedUpdate()
+    {
+        float pingPong = Mathf.PingPong(Time.fixedTime * this.platformSpeed, 1.0f);
+        var newPosition = Vector3.Lerp(this.start, this.end, pingPong);
+        this.transform.localPosition = newPosition;
+    }
+}
