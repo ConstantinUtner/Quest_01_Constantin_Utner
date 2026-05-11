c3f141311b65d2723babcedc35909e7508389437
Constantin Utner <constantin.utner@stud.hcw.ac.at>
2026-05-11 14:29:51 +0200

    Q3/HUD & UI/ Stable GameOverFadeIn

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,6 +14,36 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private Image healthBar;
 
+    [SerializeField]
+    private CanvasGroup hudCanvasGroup;
+
+    [SerializeField]
+    private CanvasGroup gameOverCanvasGroup;
+
+    [SerializeField]
+    private float fadingTime = 2.0f;
+
+    private bool isFadingInGameOver = false;
+
+    private IEnumerator FadeInGameOver()
+    {
+        this.isFadingInGameOver = true;
+        float timer = 0.0f;
+
+        while (timer < this.fadingTime)
+        {
+            float percent = timer / this.fadingTime;
+            this.hudCanvasGroup.alpha = 1.0f - percent;
+            this.gameOverCanvasGroup.alpha = percent;
+
+            yield return null;
+            timer += Time.deltaTime;
+        }
+
+        this.hudCanvasGroup.alpha = 0.0f;
+        this.gameOverCanvasGroup.alpha = 1.0f;
+    }
+
     private static UIManager instance = null;
     public static UIManager Instance => instance;
 
@@ -27,6 +58,11 @@ public class UIManager : MonoBehaviour
     {
         float percent = this.character.GetCurrentHealth() / this.character.GetMaxHealth();
         this.healthBar.fillAmount = percent;
+
+        if (percent <= 0.0f && !this.isFadingInGameOver)
+        {
+            this.StartCoroutine(this.FadeInGameOver());
+        }
     }
 
     private void Awake()
